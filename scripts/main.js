cc261d8b8051d6392dfd0ad09fcc0f9a89396fc2
ThatgirlBali <balikisolamiji50@gmail.com>
2025-06-10 02:47:14 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -3,133 +3,231 @@ const seriesList = [
     title: "4 Minutes",
     year: 2024,
     country: "Thailand",
-    image: "images/4 Minutes.jpeg",
+    image: "images/series/4 Minutes.jpeg",
     link: "seriespages/4minutes.html"
+  },
+  {
+    title: "A Breeze of Love",
+    year: 2018,
+    country: "South Korea",
+    image: "images/series/A Breeze of Love.jpeg",
+    link: "seriespages/ABreezeofLove.html"
+  },
+  {
+    title: "A Shoulder to Cry On",
+    year: 2018,
+    country: "South Korea",
+    image: "images/series/A Shoulder to Cry On.jpeg",
+    link: "seriespages/AShouldertoCryOn.html"
+  },
+  {
+    title: "About Youth",
+    year: 2018,
+    country: "South Korea",
+    image: "images/series/About Youth.jpeg",
+    link: "seriespages/About Youth.html"
+  },
+  {
+    title: "Bad Buddy",
+    year: 2018,
+    country: "Thailand",
+    image: "images/series/Bad Buddy.jpeg",
+    link: "seriespages/BadBuddy.html"
+  },
+  {
+    title: "Because of You",
+    year: 2020,
+    country: "Taiwan",
+    image: "images/series/Because of You (2020).jpeg",
+    link: "seriespages/BecauseofYou.html"
+  },
+  {
+    title: "Bed Friend",
+    year: 2018,
+    country: "Thailand",
+    image: "images/series/Bed Friend.jpeg",
+    link: "seriespages/BedFriend.html"
+  },
+  {
+    title: "Between Us",
+    year: 2018,
+    country: "Thailand",
+    image: "images/series/Between Us.jpeg",
+    link: "seriespages/BetweenUs.html"
   },
   {
     title: "Big Dragon",
     year: 2018,
     country: "Thailand",
-    image: "images/BIG DRAGON.jpeg",
+    image: "images/series/BIG DRAGON.jpeg",
     link: "seriespages/4minutes.html"
+  },
+  {
+    title: "Blue Canvas of Youthful Days",
+    year: 2018,
+    country: "Thailand",
+    image: "images/series/Blue Canvas of Youthful Days.jpeg",
+    link: "seriespages/BlueCanvasofYouthfulDays.html"
+  },
+  {
+    title: "Boys Be Brave",
+    year: 2024,
+    country: "South Korea",
+    image: "images/series/Boys Be Brave 2024.jpeg",
+    link: "seriespages/BoysBeBrave.html"
   },
   {
     title: "Century of Love",
     year: 2024,
     country: "Thailand",
-    image: "images/Century of Love.jpeg",
+    image: "images/series/Century of Love.jpeg",
     link: "seriespages/4minutes.html"
+  },
+  {
+    title: "Cherry Blossoms After Winter",
+    year: 2022,
+    country: "Thailand",
+    image: "images/series/Cherry Blossoms After Winter (2022).jpeg",
+    link: "seriespages/CherryBlossomsAfterWinter.html"
+  },
+  {
+    title: "Cherry Magic",
+    year: 2024,
+    country: "Thailand",
+    image: "images/series/cherry magic th poster.jpeg",
+    link: "seriespages/cherrymagic.html"
+  },
+  {
+    title: "Cosmetic Playlover",
+    year: 2024,
+    country: "Thailand",
+    image: "images/series/Cosmetic Playlover 2024.jpeg",
+    link: "seriespages/CosmeticPlaylover.html"
+  },
+  {
+    title: "Gameboys",
+    year: 2020,
+    country: "Phillipines",
+    image: "images/series/gameboys.jpeg",
+    link: "seriespages/gameboys.html"
+  },
+  {
+    title: "Gameboys the Movie",
+    year: 2021,
+    country: "Phillipines",
+    image: "images/series/Gameboys the Movie.jpeg",
+    link: "seriespages/GameboystheMovie.html"
   },
   {
     title: "Jack & Joker",
     year: 2024,
     country: "Thailand",
-    image: "images/Jack & Joker.jpeg",
+    image: "images/series/ack & Joker.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Kidnap",
     year: 2024,
     country: "Thailand",
-    image: "images/Kidnap.jpeg",
+    image: "images/series/Kidnap.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "KinnPorsche the Series",
     year: 2024,
     country: "Thailand",
-    image: "images/kinnporschetheseries.jpeg",
+    image: "images/series/kinnporschetheseries.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Love in The Air",
     year: 2022,
     country: "Thailand",
-    image: "images/Love in the Air.jpeg",
+    image: "images/series/Love in the Air.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "My Golden Blood",
     year: 2025,
     country: "Thailand",
-    image: "images/My golden blood.jpeg",
+    image: "images/series/My golden blood.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "NOT ME ",
     year: 2021,
     country: "Thailand",
-    image: "images/Not Me -Series.jpeg",
+    image: "images/series/Not Me -Series.jpeg",
     link: "seriespages/notme.html"
   },
   {
     title: "Only Friends",
     year: 2018,
     country: "Thailand",
-    image: "images/Only friends.jpeg",
+    image: "images/series/Only friends.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Our Skyy",
     year: 2018,
     country: "Thailand",
-    image: "images/Our Skyy.jpeg",
+    image: "images/series/Our Skyy.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Perfect 10 Liners",
     year: 2024,
     country: "Thailand",
-    image: "images/PERFECT 10 LINERS (2024).jpeg",
+    image: "images/series/PERFECT 10 LINERS (2024).jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Pit Babe",
     year: 2023,
     country: "Thailand",
-    image: "images/Pit Babe (2023).jpeg",
+    image: "images/series/Pit Babe (2023).jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Pit Babe 2",
     year: 2025,
     country: "Thailand",
-    image: "images/Pit Babe (2023).jpeg",
+    image: "images/series/Pit Babe (2023).jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Semantic Error",
     year: 2022,
     country: "South Korea",
-    image: "images/semantic error.jpeg",
+    image: "images/series/semantic error.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "The Eclipse",
     year: 2023,
     country: "Thailand",
-    image: "images/THE ECLIPSE🇹🇭.jpeg",
+    image: "images/series/THE ECLIPSE🇹🇭.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "The Heart Killers",
     year: 2024,
     country: "Thailand",
-    image: "images/THE HEART KILLERS.jpeg",
+    image: "images/series/THE HEART KILLERS.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Top Form",
     year: 2025,
     country: "Thailand",
-    image: "images/Top Form.jpeg",
+    image: "images/series/Top Form.jpeg",
     link: "seriespages/4minutes.html"
   },
   {
     title: "Until We Meet Again",
     year: 2019,
     country: "Thailand",
-    image: "images/Until We Meet Again The Series.jpeg",
+    image: "images/series/Until We Meet Again The Series.jpeg",
     link: "seriespages/4minutes.html"
   }
 ];
